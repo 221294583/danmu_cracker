c8cb2a609e90e8b36f4e90dc8c2f2d0ce822a24a
221294583 <65217044+221294583@users.noreply.github.com>
2020-08-05 20:54:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import numpy as np
 import getcomment
-import crackcrc32
+#import crackcrc32
 import visit
+import crack
 
-videocode=input("请输入视频地址:")
-list_ini=getcomment.getlist(videocode)
-list_ini.soup()
-list_ini.finder()
-list_ini.getmark()
-bullet=(input('请输入要查找的关键词:'))
+def process():
+    videocode=input("请输入视频地址:")
+    list_ini=getcomment.getlist(videocode)
+    list_ini.soup()
+    list_ini.finder()
+    list_ini.getmark()
+    bullet=(input('请输入要查找的关键词:'))
+    
+    all_bullet=list_ini.filterconcrete(bullet)
+    
+    all_crc32s=list_ini.getcrc32()
+    
+    #crackcrc32.create_table()
+    crack.main()
+    
+    all_uid=[]
+    for ch in all_crc32s:
+        temp=crack.crackl4(ch)[0]
+        #print(type(temp))
+        all_uid.append(temp)
+    
+    all_space=[]
+    for ch in all_uid:
+        temp=['http://space.bilibili.com/',ch]
+        buffer=''.join(temp)
+        all_space.append(buffer)
+    
+    user_nickname=visit.user_info(all_space)
+    nicknames=user_nickname.getnickname()
+    print('弹幕信息以及发送者信息:','\n')
+    for i in range(len(all_bullet)):
+        temp=['http://space.bilibili.com/',all_uid[i]]
+        print('弹幕内容:',' ',all_bullet[i],'   ','用户昵称:',' ',nicknames[i],'   ','空间网址:',' ',(''.join(temp)))
 
-all_bullet=list_ini.filterconcrete(bullet)
-print(all_bullet)
-
-all_crc32s=list_ini.getcrc32()
-print(all_crc32s)
-
-crackcrc32.create_table()
-
-all_uid=[]
-for ch in all_crc32s:
-    temp=crackcrc32.main(ch)
-    all_uid.append(temp)
-print(all_uid)
-
-all_space=[]
-for ch in all_uid:
-    temp=['http://space.bilibili.com/',ch]
-    buffer=''.join(temp)
-    all_space.append(buffer)
-print(all_space)
-
-user_nickname=visit.user_info(all_space)
-nicknames=user_nickname.getnickname()
+    return all_bullet, all_crc32s, all_space, all_uid, bullet, ch, i, list_ini, nicknames, user_nickname, videocode
